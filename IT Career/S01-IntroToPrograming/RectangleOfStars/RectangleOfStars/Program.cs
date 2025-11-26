eb4c9b02fa0bd65da27328045d25b7959072b319
Vanko09 <vanko09@abv.bg>
2025-11-26 22:05:45 +0200

    IT Career

---
+++ IT Career/S01-IntroToPrograming/RectangleOfStars/RectangleOfStars/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static void function(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write("*");
+        }
+    }
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        function(n);
+        Console.WriteLine();
+        for (int i = 2; i < n; i++)
+        {
+            Console.Write("*");
+            for (int j = 1; j < n - 1; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("*");
+        }
+        function(n);
+    }
+}
---
+++ IT Career/S01-IntroToPrograming/TriangleOf55Stars/TriangleOf55Stars/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write('*');
+            }
+            Console.WriteLine();
+        }
+    }
+}
