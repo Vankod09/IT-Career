91631325aec4331c386b88942c446bed5cd35a8e
Vanko09 <vanko09@abv.bg>
2025-12-01 17:28:46 +0200

    IT Career

---
+++ IT Career/M01-IntroToPrograming/S04-Loops/P08-RectangleOfNStars/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(new string('*', n));
+        }
+    }
+}
