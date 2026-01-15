487676a46ffeee8f10c0c3fd9b14389e8efed613
Vanko09 <vankod09@gmail.com>
2026-01-15 14:55:03 +0200

    IT Career

--- IT Career/M01-IntroToPrograming/S01-IntroToPrograming/P02-RectangleSquare/RectangleSquare/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Program
-{
-    public static void Main(string[] args)
-    {
-        int a = int.Parse(Console.ReadLine());
-        int b = int.Parse(Console.ReadLine());
-        Console.WriteLine(a * b);
-    }
-}
